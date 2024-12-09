d6a48292a149ee00133a6fee32e899304a43c4fa
Theerthagiri Soundararajan <49114091+giritheerth@users.noreply.github.com>
2024-12-09 21:45:33 +1100

    init

---
+++ Basics/basics.py
@@ -0,0 +1,22 @@
+import keyword
+
+# print all the available keywords in python
+print(f"Keywords in python: {keyword.kwlist}")
+
+# variable - container for a value ( String, Integer, float, boolean)
+first_name = "Theerthagiri"
+age = 35
+iphone_rate = 1200.99
+amazing = True
+print(
+    f"Welcome {first_name}, your age is {age} and you have bought  iphone worth {iphone_rate} which is {amazing}ly amazing!")
+
+# Typecasting- converting one variable to another variable (str(),int(),float(),bool())
+name = "Saraniyaa"
+age = 34
+gpa = 3.65
+is_available = True
+print(type(name))
+
+# input() - prompts user to input function and always returns entered data in string format
+# taking multiple inputs from the user
