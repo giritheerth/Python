403bed9b1a9a2dd68e2a6f6406d09d8e38eb4169
giri <giri@smartpropertyowners.com>
2024-12-11 14:14:40 +1100

    init

--- Basics/basics.py
+++ Basics/basics.py
@@ -9,7 +9,7 @@ age = 35
 iphone_rate = 1200.99
 amazing = True
 print(
-    f"Welcome {first_name}, your age is {age} and you have bought  iphone worth {iphone_rate} which is {amazing}ly amazing!")
+    f"Welcome {first_name}, your age is {age} and you have bought  iphone worth ${iphone_rate} which is {amazing}ly amazing!")
 
 # Typecasting- converting one variable to another variable (str(),int(),float(),bool())
 name = "Saraniyaa"
@@ -17,6 +17,12 @@ age = 34
 gpa = 3.65
 is_available = True
 print(type(name))
+print(float(age))
+print(int(gpa))
 
 # input() - prompts user to input function and always returns entered data in string format
 # taking multiple inputs from the user
+
+
+
+
