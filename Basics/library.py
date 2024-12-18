c6088cd15dcc709f4f795026ed7cc1ee37b44770
Theerthagiri Soundararajan <49114091+giritheerth@users.noreply.github.com>
2024-12-18 11:17:37 +1100

    init

--- Basics/library.py
+++ Basics/library.py
@@ -1,1 +1,15 @@
-import matplotlib.pyplot
+# matplot lib
+'''
+1. various data visualization task, exploratory data analysis, scientific plotting and creating publication-quality plots.
+2. good for scenario where we need fine grained controls over plot customization and creat specialized visualizations.
+3. cross platform and easy integration with numpy.
+
+cons: limited 3D plotting, dependencies on external libraries
+
+Apps: scientific research, finance, data analysis and education.
+
+Layers:
+1. scripting layer- top level - simple plots to mimic MATLAB plotting commands.
+2. artist layer- full control and appearance of the plot.
+3. background layer- lowest level - how to render and how to use different GUI ex: pyQT
+'''
